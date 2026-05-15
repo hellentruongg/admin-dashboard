6a82b151658e921622822b1a16a87b63108a8c37
hellen truong <hellentruong123@gmail.com>
2026-05-15 16:06:50 +0200

    style: add base styles

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -2,10 +2,11 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../components/navbar/Navbar";
 import Sidemenu from "../components/sidemenu/Sidemenu";
 import Footer from "../components/footer/Footer";
+import "../styles/global.scss";
 
 const RootLayout = () => {
   return (
-    <div className="container">
+    <div className="wrapper">
       <header>
         <Navbar />
       </header>
