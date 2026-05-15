2599fa8f38f994e17eb59d6948a762a5c76cb351
hellen truong <hellentruong123@gmail.com>
2026-05-15 14:18:33 +0200

    fix: remove import of deleted file

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router.tsx";
 
-import "./index.css";
 // import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
