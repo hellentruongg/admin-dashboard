c45843dac864ea9b0413c7eb3f14b6da41f0970c
hellen truong <hellentruong123@gmail.com>
2026-05-19 15:37:26 +0200

    feat: create navbar component

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,26 @@
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <div>
+        <img src="#" />
+        <span>Dashboard</span>
+      </div>
+      <div>
+        <img src="/search.svg" />
+        <img src="/app.svg" />
+        <img src="/expand.svg" />
+        <div>
+          <img src="/notification.svg" />
+          <span>1</span>
+        </div>
+        <div>
+          <img src="https://images.unsplash.com/photo-1586299485759-f62264d6b63f?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+          <span>Jane</span>
+        </div>
+        <img src="/settings.svg" />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
