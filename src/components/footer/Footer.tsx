e364e94304cad888f1a9700c930d121f0c3a5e8d
hellen truong <hellentruong123@gmail.com>
2026-05-20 12:11:43 +0200

    style: adjust footer items typography

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,8 +3,8 @@ import "./Footer.scss";
 const Footer = () => {
   return (
     <footer className="footer">
-      <span>Dashboard</span>
-      <span>&copy; 2026 Admin Dashboard</span>
+      <span className="footer__logo">Dashboard</span>
+      <span className="footer__copyright">&copy; 2026 Admin Dashboard</span>
     </footer>
   );
 };
