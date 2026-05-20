f70f08c2ea19a37d4cd9d5acd81761fb7502ff72
hellen truong <hellentruong123@gmail.com>
2026-05-20 13:16:00 +0200

    feat: add initial html structure

--- src/components/sidemenu/Sidemenu.tsx
+++ src/components/sidemenu/Sidemenu.tsx
@@ -1,5 +1,27 @@
+import { Link } from "react-router-dom";
+
 const Sidemenu = () => {
-  return <div>Sidemenu</div>;
+  return (
+    <nav>
+      <section>
+        <h3>Main</h3>
+        <ul>
+          <li>
+            <Link to="#">
+              <img src="#" />
+              <span>Home</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="#">
+              <img src="#" />
+              <span>Profile</span>
+            </Link>
+          </li>
+        </ul>
+      </section>
+    </nav>
+  );
 };
 
 export default Sidemenu;
