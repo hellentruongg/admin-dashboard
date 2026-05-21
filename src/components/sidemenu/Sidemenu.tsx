ce0f4f40c2679b2640567469cade1457486639ef
hellen truong <hellentruong123@gmail.com>
2026-05-21 16:34:56 +0200

    feat: render menu list and items from data

--- src/components/sidemenu/Sidemenu.tsx
+++ src/components/sidemenu/Sidemenu.tsx
@@ -1,26 +1,25 @@
 import { Link } from "react-router-dom";
 import "./Sidemenu.scss";
+import { menu } from "../../data/menu";
 
 const Sidemenu = () => {
   return (
     <nav className="menu">
-      <section className="menu__section">
-        <h3 className="menu__title">Main</h3>
-        <ul className="menu__list">
-          <li className="menu__item">
-            <Link to="#" className="menu__link">
-              <img src="#" />
-              <span>Home</span>
-            </Link>
-          </li>
-          <li className="menu__item">
-            <Link to="#" className="menu__link">
-              <img src="#" />
-              <span>Profile</span>
-            </Link>
-          </li>
-        </ul>
-      </section>
+      {menu.map((list) => (
+        <section className="menu__section" key={list.id}>
+          <h3 className="menu__title">{list.title}</h3>
+          <ul className="menu__list">
+            {list.listItems.map((item) => (
+              <li className="menu__item" key={item.id}>
+                <Link to="{item.url}" className="menu__link">
+                  <img src={item.icon} />
+                  <span>{item.title}</span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </section>
+      ))}
     </nav>
   );
 };
