0dd80473dd596d67d753be2c96fa9cc48772f604
hellen truong <hellentruong123@gmail.com>
2026-05-21 22:16:02 +0200

    refactor: implement markup for dashboard layout

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,19 @@
 import "./Home.scss";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="overview">
+      <article className="overview__card overview__1"></article>
+      <article className="overview__card overview__2"></article>
+      <article className="overview__card overview__3"></article>
+      <article className="overview__card overview__4"></article>
+      <article className="overview__card overview__5"></article>
+      <article className="overview__card overview__6"></article>
+      <article className="overview__card overview__7"></article>
+      <article className="overview__card overview__8"></article>
+      <article className="overview__card overview__9"></article>
+    </div>
+  );
 };
 
 export default Home;
