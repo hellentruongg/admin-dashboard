c08fe826eb6aab622d59e7450cb3d450cb4cdec7
hellen truong <hellentruong123@gmail.com>
2026-05-22 14:03:20 +0200

    style: add styling to overview cards

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,15 +3,15 @@ import "./Home.scss";
 const Home = () => {
   return (
     <div className="overview">
-      <article className="overview__card overview__1"></article>
-      <article className="overview__card overview__2"></article>
-      <article className="overview__card overview__3"></article>
-      <article className="overview__card overview__4"></article>
-      <article className="overview__card overview__5"></article>
-      <article className="overview__card overview__6"></article>
-      <article className="overview__card overview__7"></article>
-      <article className="overview__card overview__8"></article>
-      <article className="overview__card overview__9"></article>
+      <article className="overview__card overview__1">Card 1</article>
+      <article className="overview__card overview__2">Card 3</article>
+      <article className="overview__card overview__3">Card 3</article>
+      <article className="overview__card overview__4">Card 4</article>
+      <article className="overview__card overview__5">Card 5</article>
+      <article className="overview__card overview__6">Card 6</article>
+      <article className="overview__card overview__7">Card 7</article>
+      <article className="overview__card overview__8">Card 8</article>
+      <article className="overview__card overview__9">Card 9</article>
     </div>
   );
 };
