b9306b94ee6a243fbe8badfde812800e7bfd7385
hellen truong <hellentruong123@gmail.com>
2026-05-25 11:46:06 +0200

    refactor: improve semantic HTML structure

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,17 +4,17 @@ import "./Home.scss";
 const Home = () => {
   return (
     <div className="overview">
-      <article className="overview__card overview__1">
+      <div className="overview__card overview__1">
         <Toplist />
-      </article>
-      <article className="overview__card overview__2">Card 2</article>
-      <article className="overview__card overview__3">Card 3</article>
-      <article className="overview__card overview__4">Card 4</article>
-      <article className="overview__card overview__5">Card 5</article>
-      <article className="overview__card overview__6">Card 6</article>
-      <article className="overview__card overview__7">Card 7</article>
-      <article className="overview__card overview__8">Card 8</article>
-      <article className="overview__card overview__9">Card 9</article>
+      </div>
+      <div className="overview__card overview__2">Card 2</div>
+      <div className="overview__card overview__3">Card 3</div>
+      <div className="overview__card overview__4">Card 4</div>
+      <div className="overview__card overview__5">Card 5</div>
+      <div className="overview__card overview__6">Card 6</div>
+      <div className="overview__card overview__7">Card 7</div>
+      <div className="overview__card overview__8">Card 8</div>
+      <div className="overview__card overview__9">Card 9</div>
     </div>
   );
 };
